de5b051bd634b3181882e83c634e733e24f95a76
Denis <denisgaldin@mail.ru>
2024-10-02 21:50:30 +0300

    test_dark_theme_by_time update

--- homework.py
+++ homework.py
@@ -1,15 +1,27 @@
 from datetime import time
 
+from trio import current_time
+
 
 def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
-    current_time = time(hour=23)
-    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
-    assert is_dark_theme is True
+
+current_time = time(hour=23)
+dark_theme_enabled_by_user = True
+
+if dark_theme_enabled_by_user:
+    is_dark_theme = True
+elif dark_theme_enabled_by_user is None:
+    if time(22) <= current_time < time(6):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
+else:
+    is_dark_theme = False
+assert is_dark_theme is True
 
 
 def test_dark_theme_by_time_and_user_choice():
