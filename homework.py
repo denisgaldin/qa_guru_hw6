3b6677d4f6e386cf77f4f4c9d69d8a76b0ec86c8
denisgaldin <denisgaldin@mail.ru>
2024-10-04 18:36:07 +0300

    Update

--- homework.py
+++ homework.py
@@ -24,18 +24,11 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
+
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
 
-    if dark_theme_enabled_by_user:
-        is_dark_theme = True
-    elif dark_theme_enabled_by_user is None:
-        if time(22) <= current_time or current_time < time(6):
-            is_dark_theme = True
-        else:
-            is_dark_theme = False
-    else:
-        is_dark_theme = False
+    is_dark_theme = dark_theme_enabled_by_user or (time(22) <= current_time or current_time < time(6))
 
     assert is_dark_theme is True
 
@@ -80,45 +73,36 @@ def test_find_suitable_user():
 
 
 def print_readable_function_name(func, *args, **kwargs):
-    # Получаем имя функции и заменяем подчеркивания на пробелы
     func_name = func.__name__.replace('_', ' ').title()
 
-    # Формируем строку с аргументами
-    arg_list = []
-    for arg in args:
-        arg_list.append(str(arg))
-
-    for key, value in kwargs.items():
-        arg_list.append(f"{key}={value}")
-
+    arg_list = [str(arg) for arg in args] + [f"{key}={value}" for key, value in kwargs.items()]
     args_string = ", ".join(arg_list)
 
-    # Выводим результат
-    print(f"{func_name} [{args_string}]")
+    result = f"{func_name} [{args_string}]"
+
+    print(result)
+
+    return result
 
 
 def test_readable_function():
-    open_browser(browser_name="Chrome")
-    go_to_companyname_homepage(page_url="https://companyname.com")
-    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+    assert open_browser(browser_name="Chrome") == "Open Browser [Chrome]"
+    assert go_to_companyname_homepage(
+        page_url="https://companyname.com") == "Go To Companyname Homepage [https://companyname.com]"
+    assert find_registration_button_on_login_page(page_url="https://companyname.com/login",
+                                                  button_text="Register") == "Find Registration Button On Login Page [https://companyname.com/login, button_text=Register]"
 
 
 def open_browser(browser_name):
-    print_readable_function_name(open_browser, browser_name)
-    actual_result = f"Open Browser [{browser_name}]"
-    expected_result = "Open Browser [Chrome]"
-    assert actual_result == expected_result, f"Ожидалось: {expected_result}, но получено: {actual_result}"
+    return print_readable_function_name(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-    print_readable_function_name(go_to_companyname_homepage, page_url)
-    actual_result = f"Go To Companyname Homepage [{page_url}]"
-    expected_result = "Go To Companyname Homepage [https://companyname.com]"
-    assert actual_result == expected_result, f"Ожидалось: {expected_result}, но получено: {actual_result}"
+    return print_readable_function_name(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    print_readable_function_name(find_registration_button_on_login_page, page_url, button_text=button_text)
-    actual_result = f"Find Registration Button On Login Page [{page_url}, {button_text}]"
-    expected_result = "Find Registration Button On Login Page [https://companyname.com/login, Register]"
-    assert actual_result == expected_result, f"Ожидалось: {expected_result}, но получено: {actual_result}"
+    return print_readable_function_name(find_registration_button_on_login_page, page_url, button_text=button_text)
+
+
+test_readable_function()
